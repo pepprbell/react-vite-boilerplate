434fc87fb95a1f4a5506507c0b335bb7ee366222
pepprbell <pepprbell@gmail.com>
2024-11-14 19:26:00 +0900

    add: vite resolve / local port setting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+    ]
+  },
+  // local port setting
+  // server: {
+  //   port: 5273,
+  // },
 })
